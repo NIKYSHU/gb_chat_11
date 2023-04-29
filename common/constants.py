2b49dd9c1291947f6446cae4386e3316b6e6fd31
NIKYSHU <nikyshu@gmail.com>
2023-04-29 10:18:32 +0300

    HW_lesson_8

---
+++ common/constants.py
@@ -0,0 +1,31 @@
+# Default server port
+DEFAULT_PORT = 7777
+
+# Default server IP
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+
+# Maximum connections
+MAX_CONNECTIONS = 5
+
+# Max length of package
+MAX_PACKAGE_LENGTH = 1024
+
+# Default encoding
+ENCODING = 'utf-8'
+
+# Default JIM Protocol
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+DESTINATION = 'to'
+SENDER = 'from'
+
+# Other
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+
+MESSAGE = 'msg'
+MESSAGE_TEXT = 'msg txt'
+EXIT = 'exit'
